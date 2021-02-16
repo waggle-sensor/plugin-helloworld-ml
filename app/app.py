dc31c7c985f926edfc78afacbbe19dde21e4d336
Robert Jackson <rjackson@compute-386-01.cels.anl.gov>
2021-02-16 09:12:29 -0600

    ADD: Model

--- app/app.py
+++ app/app.py
@@ -1,97 +1,105 @@
+import numpy as np
+import xgboost as xgb
+import xarray as xr
+import act
+import highiq
+import pandas as pd
 import time
 import datetime
 import argparse
 import multiprocessing
+
 from itertools import compress
-
-import cv2
-import numpy as np
-import tflite_runtime.interpreter as tflite
-
-
-def open_video_capture(input):
-    capture = cv2.VideoCapture(input)
-
-    if not capture.isOpened():
-        raise RuntimeError(f'could not open input {input}')
-
-    return capture
+from glob import glob
 
 
 def open_load_model(model_path):
-    interpreter = tflite.Interpreter(model_path)
-    interpreter.allocate_tensors()
-    return interpreter
+    bst = xgb.load_model(model_path)
+    return bst
 
+def load_file(file_path):
+    test_file = highiq.io.load_arm_netcdf(file_path)
+    return test_file
 
+def process_file(ds):
+    ds_out = highiq.calc.get_psd(ds)
+    ds_out = highiq.calc.get_lidar_moments(ds_out)
+    return ds_out
+
+def get_scp(ds, model_name, interval=5):
+    range_bins = np.arange(60., 11280., 120.)
+    # Parse model string for locations of snr, mean_velocity, spectral_width
+    locs = 0
+    snr_thresholds = []
+    scp_ds = {}
+    dates = pd.date_range(ds.time.values[0],
+                          ds.time.values[-1], freq='%dmin' % interval)
+    while locs > -1:
+        locs = model_name.find("snr")
+        if locs > -1:
+            snr_thresholds.append(float(model_name[locs+5:locs+15]))
+            scp_ds['snrgt%f' % snr_thresholds[-1]] = np.zeros(len(dates) - 1, len(times) - 1)
+
+    for i in range(len(dates)-1):
+        time_inds = np.argwhere(np.logical_and(ds.time.values >= dates[i],
+                                               ds.time.values < dates[i+1]))
+        for j in range(len(range_bins) - 1):
+            range_inds = np.argwhere(np.logical_and(
+                ds.range.values >= range_bins[j], ds.range.values < range_bins[j+1]))
+            snrs = ds['snr'].values[time_inds, range_inds]
+            for snr_thresh in snr_thresholds:
+                scp_ds['snrgt%f' % snr_thresh][i, j] = len(np.argwhere(snrs > snr_thresh)) / \
+                                                       (len(time_inds) * len(range_inds))
+    scp_ds['input_array'] = np.concatenate([scp_ds[var_keys] in scp_ds.keys()], axis=1)
+
+    scp_ds['time_bins'] = dates
+    return scp_ds
+        
+def download_data(date, time):
+    act.discovery.download_data('rjackson', '3326641ebc6b55aa',
+                                'sgpdlacfC1.a1', date, date, time)
+    
 def worker_main(args, heartbeat):
     interval = int(args.interval)
     print(f'opening input {args.input}', flush=True)
-    capture = open_video_capture(args.input)
+    
+    class_names = ['clear', 'cloudy', 'rain']
+    # First get the time period using act
 
-    # bind read_image function to capture
-    def read_image():
+    if args.time is None:
+        file_list = glob('sgpdlacfC1.a1.%s*.nc' % args.date)
+        file_name = file_list[-1]
+    else:
+        file_name = 'sgpdlacfC1.a1.%s.%s.nc' % (args.date, args.time)
+
+    input_ds = load_file(file_name)
+
+    model = open_load_model(args.model)
+    if args.date == datetime.date.today().strftime('%Y%m%d') and time is None:
         while True:
-            _, img = capture.read()
-            if img is not None:
-                return img
-            time.sleep(0.1)
-
-    # inference
-    def inferencing(interpreter, input_index, output_index, input_data):
-        interpreter.set_tensor(input_index, input_data)
-        interpreter.invoke()
-        return interpreter.get_tensor(output_index)
-
-    # softmax in numpy
-    def softmax(x):
-        e_x = np.exp(x)
-        return e_x / e_x.sum()
-
-    print('loading model and model config', flush=True)
-    interpreter = open_load_model(args.model)
-    input_details = interpreter.get_input_details()
-    input_index = input_details[0]['index']
-    output_details = interpreter.get_output_details()
-    output_index = output_details[0]['index']
-
-    class_names = np.array(['WithoutMask', 'WithMask'])
-
-    while True:
-        heartbeat.put(1)
-
-        if args.verbose:
-            print('reading image...', flush=True)
-        image = read_image()
-        if args.verbose:
-            print('read image', flush=True)
-
-        image_resized = cv2.resize(image, (224, 224))
-        image_rgb = cv2.cvtColor(image_resized, cv2.COLOR_BGR2RGB)
-        input_data = np.expand_dims(image_rgb, axis=0)
-        input_data = input_data.astype(np.float32)
-
-        if args.verbose:
-            start_t = time.time()
-        output_data = inferencing(interpreter, input_index, output_index, input_data)
-        
-        if args.verbose:
-            print('{:.2f} elapsed for inferencing'.format(time.time() - start_t), flush=True)
-
-        results = np.squeeze(output_data)
-        probabilities = softmax(results)
-
-        top_5 = results.argsort()[-5:][::-1]
-        print('results(prob: class):', flush=True)
-        for i in top_5:
-            print('{:8.6f}: {}'.format(
-                probabilities[i],
-                class_names[i]), flush=True)
-        
-        if interval > 0:
-            if args.verbose:
-                print('sleeping for {} seconds'.format(interval), flush=True)
-            time.sleep(interval)
+            file_list = glob('sgpdlacfC1.a1.%s*.nc' % args.date)
+            if file_list == []:
+                download_data(args.date)
+            file_list = glob('sgpdlacfC1.a1.%s*.nc' % args.date)
+            file_name = file_list[-1]
+            dsd_ds = process_file(input_ds)
+            scp = get_scp(dsd_ds)
+            input_ds.close()
+            dsd_ds.close()
+            out_predict = model.predict(scp['input_array'])
+            for i in range(len(out_predict)):
+                print(scp['time_bins'][i] + ':' + class_names[int(out_predict[i])])
+    else:
+        download_data(args.date, args.time)
+        file_list = glob('sgpdlacfC1.a1.%s*.nc' % args.date)
+        file_name = file_list[-1]
+        dsd_ds = process_file(input_ds)
+        scp = get_scp(dsd_ds)
+        input_ds.close()
+        dsd_ds.close()
+        out_predict = model.predict(scp['input_array'])
+        for i in range(len(out_predict)):
+            print(scp['time_bins'][i] + ':' + class_names[int(out_predict[i])])
 
 
 def main(args):
@@ -102,6 +110,8 @@ def main(args):
     worker = multiprocessing.Process(
         target=worker_main, args=(args, heartbeat))
 
+    model = open_load_model(args.model)
+    
     try:
         worker.start()
         while True:
@@ -121,19 +131,21 @@ if __name__ == '__main__':
         action='store_true', help='Verbose')
     parser.add_argument(
         '-input', dest='input',
-        action='store', default='/dev/video0',
+        action='store', default='sgpdlacfC1.a1',
         help='Path to input device or stream')
     parser.add_argument(
         '-model', dest='model',
-        action='store', default='mask_classifier_mobilenetv2_224_tuned.tflite',
+        action='store', default='modelsnrgt3.000000snrgt5.000000.json',
         help='Path to model')
-    parser.add_argument(
-        '-image-size', dest='image_size',
-        action='store', default=224,
-        help='Input size of the model')
     parser.add_argument(
         '-interval', dest='interval',
         action='store', default=0,
         help='Time interval in seconds')
+    parser.add_argument('-date', dest='date', action='store',
+                        default=datetime.date.today().strftime('%Y%m%d'),
+                        help='Date of record to pull')
+    parser.add_argument('-time', dest='time', action='store',
+                        default=None, help='Time of record to pull')
+
 
     main(parser.parse_args())
