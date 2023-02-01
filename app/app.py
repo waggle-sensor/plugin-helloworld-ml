ab5255b6161d9630c50d3384dbd47c52cdcc16fd
Robert Jackson <rjackson@evswl137.evs.anl.gov>
2023-02-01 15:59:27 -0600

    Latest changes

--- app/app.py
+++ app/app.py
@@ -1,5 +1,4 @@
 import numpy as np
-import xgboost as xgb
 import xarray as xr
 import pandas as pd
 import highiq
@@ -8,18 +7,24 @@ import argparse
 import os
 import base64
 import paramiko
-import json
 import xarray as xr
-import io
+import tensorflow as tf
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
 
 from waggle.plugin import Plugin
 from datetime import datetime, timedelta
-
+from scipy.signal import convolve2d
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from glob import glob
 # 1. import standard logging module
 import logging
 
 # 2. enable debug logging
-#logging.basicConfig(level=logging.DEBUG)
+
+logging.basicConfig(level=logging.DEBUG)
 
 def open_load_model(model_path):
     print(model_path)
@@ -28,11 +33,11 @@ def open_load_model(model_path):
     return bst
 
 def load_file(file_path):
-    test_file = highiq.io.read_00_data(file_path, 'sgpdlprofcalC1.home_point')
-    test_file.to_netcdf('test.nc')
-    del test_file
-    test_file = xr.open_dataset('test.nc')
+    test_file = xr.open_dataset(file_path)
     return test_file
+
+def return_convolution_matrix(time_window, range_window):
+    return np.ones((time_window, range_window)) / (time_window * range_window)
 
 def process_file(ds):
     print("Processing lidar moments...")
@@ -50,133 +55,173 @@ def process_file(ds):
     return ds_out
 
 
-def get_scp(ds, model_name, config, interval=5):
+def make_imgs(ds, config, interval=5):
     range_bins = np.arange(60., 11280., 120.)
     # Parse model string for locations of snr, mean_velocity, spectral_width
     locs = 0
     snr_thresholds = []
     scp_ds = {}
-    if config == "Stare":
-        interval = 5
-        dates = pd.date_range(ds.time.values[0],
-                          ds.time.values[-1], freq='%dmin' % interval)
-    else:
-        dates = pd.date_range(ds.time.values[0],
-                          ds.time.values[-1], periods=2)
+    interval = 5
+    dates = pd.date_range(ds.time.values[0], ds.time.values[-1], freq='%dmin' % interval)
 
     times = ds.time.values
-    snr = ds['snr'].values
-    mname = model_name
-    while locs > -1:
-        locs = mname.find("snr")
-        if locs > -1:
-            snr_thresholds.append(float(mname[locs+5:locs+13]))
-            scp_ds['snrgt%f' % snr_thresholds[-1]] = np.zeros(
-                    (len(dates) - 1, len(range_bins) - 1))
-            mname = mname[locs+13:]
-    
-    for i in range(len(dates) - 1):
-        time_inds = np.argwhere(np.logical_and(ds.time.values >= dates[i],
-                                               ds.time.values < dates[i + 1]))
-        if len(time_inds) == 0:
-            continue
-        for j in range(len(range_bins) - 1):
-            range_inds = np.argwhere(np.logical_and(
-                ds.range.values >= range_bins[j], 
-                ds.range.values < range_bins[j+1]))
-            range_inds = range_inds.astype(int)
-            if len(range_inds) == 0:
-                continue
-            snrs = snr[int(time_inds[0]):int(time_inds[-1]), 
-                    int(range_inds[0]):int(range_inds[-1])]
-            for snr_thresh in snr_thresholds:
-                scp_ds['snrgt%f' % snr_thresh][i, j] = len(np.argwhere(snrs > snr_thresh)) / \
-                                                       (len(time_inds) * len(range_inds)) * 100
-    scp_ds['input_array'] = np.concatenate(
-            [scp_ds[var_keys] for var_keys in scp_ds.keys()], axis=1)
-    scp_ds['time_bins'] = dates
-    print(scp_ds['input_array'].shape)
-    return scp_ds
+    print(times)
+    ranges = ds.range.values
+    ds['snr'] = ds['snr'] + 2 * np.log10(ranges + 1)
+    conv_matrix = return_convolution_matrix(5, 5)
+    snr_avg = convolve2d(ds['snr'].values, conv_matrix, mode='same')
+    ds['stddev'] = (('time', 'range'), np.sqrt(convolve2d(
+        (ds['snr'] - snr_avg) ** 2, conv_matrix, mode='same')))
+    Zn = ds.stddev.values
+    cur_time = times[0]
+    end_time = times[-1]
+    time_list = []
+    start_ind = 0
+    i = 0
+    first_shape = None
+    while cur_time < end_time:
+        next_time = cur_time + np.timedelta64(interval, 'm')
+        if next_time > end_time:
+            next_ind = len(times)
+        else:
+            next_ind = np.argmin(np.abs(next_time - times))
+        if (start_ind >= next_ind):
+            break
+        my_data = Zn[start_ind:next_ind, 0:150].T
+        my_times = times[start_ind:next_ind]
+        if len(my_times) == 0:
+            break
+        start_ind += next_ind - start_ind + 1
+
+        if first_shape is None:
+            first_shape = my_data.shape
+        else:
+            if my_data.shape[0] > first_shape[0]:
+                my_data = my_data[:first_shape[0], :]
+            elif my_data.shape[0] < first_shape[0]:
+                my_data = np.pad(my_data, [(0, first_shape[0] - my_data.shape[0]), (0, 0)],
+                                 mode='constant')
+
+        if not os.path.exists('/app/imgs/'):
+            os.mkdir('/app/imgs')
+
+        fname = '/app/imgs/%d.png' % i
+        width = first_shape[0]
+        height = first_shape[1]
+        # norm = norm.SerializeToStri
+        fig, ax = plt.subplots(1, 1, figsize=(1 * (height / width), 1))
+        # ax.imshow(my_data)
+        ax.pcolormesh(my_data, cmap='act_HomeyerRainbow', vmin=20, vmax=150)
+        ax.set_axis_off()
+        ax.margins(0, 0)
+        fig.savefig(fname, dpi=300, bbox_inches='tight', pad_inches=0)
+        plt.close(fig)
+        i = i + 1
+        del fig, ax
+        time_list.append(cur_time)
+        cur_time = next_time
+
+    return time_list
 
 
 def progress(bytes_so_far: int, total_bytes: int):
     pct_complete = 100. * float(bytes_so_far) / float(total_bytes)
     if int(pct_complete * 10) % 100 == 0:
-        print("Total progress = %4.2f" % pct_complete)  
+        print("Total progress = %4.2f" % pct_complete)
 
 
 def download_data(args, file_name):
     bt = time.time()
-    passwd = base64.b64decode("S3VyQGRvMjM=".encode("utf-8"))
-    transport = paramiko.Transport('research.adc.arm.gov', 22)
-    username = 'rjackson'
+    #passwd = base64.b64decode("S3VyQGRvMjM=".encode("utf-8"))
+    passwd = 'Kur@do43c'
+    jump_host = 'portal.arm.gov'
+    research_host = 'emerald.adc.arm.gov'
+    logging.debug("Opening connection...")
+    with paramiko.SSHClient() as jumpbox:
+        jumpbox.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        jumpbox.connect(jump_host, username='rjackson', password=passwd)
+        jump_transport = jumpbox.get_transport()
+        src_addr = (jump_host, 22)
+        dest_addr = (research_host, 22)
+        jumpbox_channel = jump_transport.open_channel("direct-tcpip",
+                dest_addr=dest_addr, src_addr=src_addr)
 
-    transport.connect(username=username, password=passwd)
-    return_list = []
-    with paramiko.SFTPClient.from_transport(transport) as sftp:
-        sftp.chdir('/data/datastream/sgp/%s' % args.input)
-        if args.date is None and args.time is None:
-            file_list = sorted(sftp.listdir())[-5:]
-        elif args.time is None:
-            file_list = sorted(sftp.listdir())
-            for f in file_list:
-                if not args.date in f:
-                    file_list.remove(f)
-        else:
-            file_list = ['%s.%s.%s.nc' % (args.input, args.date, args.time)]
-        last_file = ""
-        for f in file_list:
-            print(f)
-            if os.path.exists('/app/%s' % f):
-                continue
-            
-            # Only process vertically pointing data for now
-            if not args.config in f:
-                continue
-            print("Downloading %s" % f)
-            return_list = '/app/%s' % f
-            last_file = f
-        if last_file == file_name:
-            return []
-        sftp.get(last_file, localpath='/app/%s' % last_file, callback=progress)
+        with paramiko.SSHClient() as target:
+            target.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            target.connect(research_host, username='rjackson',
+                    password=passwd, sock=jumpbox_channel)
+            transport = target.get_transport()
 
-    transport.close()
-    print("Download done in %3.2f minutes" % ((time.time() - bt)/60.0))
-    return return_list
+            return_list = []
+            with paramiko.SFTPClient.from_transport(transport) as sftp:
+                sftp.chdir('/data/datastream/sgp/%s' % args.input)
+                if args.date is None and args.time is None:
+                    file_list = sorted(sftp.listdir())[-5:]
+                elif args.time is None:
+                    file_list = sorted(sftp.listdir())
+                    for f in file_list:
+                        if not args.date in f:
+                            file_list.remove(f)
+                else:
+                    file_list = ['%s.%s.%s.nc' % (args.input, args.date, args.time)]
+                last_file = ""
+                for f in file_list:
+                    print(f)
+                    if os.path.exists('/app/%s' % f):
+                        continue
 
+                    # Only process vertically pointing data for now
+                    if not args.config in f:
+                        continue
+                    logging.debug("Found %s" % f)
+                    return_list = '/app/%s' % f
+
+                    last_file = f
+                if last_file == file_name:
+                    return []
+                logging.debug("Downloading %s" % last_file)
+                sftp.get(file_list[-1], localpath='/app/%s' % last_file, callback=progress)
+            transport.close()
+
+    logging.debug("Download done in %3.2f minutes" % ((time.time() - bt)/60.0))
+    return last_file
 
 def worker_main(args):
     interval = int(args.interval)
-    print('opening input %s' % args.input)
+    logging.debug('opening input %s' % args.input)
     old_file = ""
     run = True
     with Plugin() as plugin:
         while run:
-            class_names = ['clear', 'cloudy', 'rain']
+            class_names = ['clear', 'cloudy']
             file_name = download_data(args, old_file)
             if file_name == []:
                 time.sleep(180)
                 continue
-            model = open_load_model(args.model)
-            print("Processing %s" % file_name)
+            model = load_model(args.model)
+            logging.debug("Processing %s" % file_name)
             dsd_ds = load_file(file_name)
             dsd_ds = process_file(dsd_ds)
             if args.config == "User5":
                 dsd_ds["range"] = dsd_ds["range"] * np.sin(np.pi / 3)
-            scp = get_scp(dsd_ds, args.model, args.config)
-            out_predict = model.predict(xgb.DMatrix(scp['input_array']))
+            time_list = make_imgs(dsd_ds, args.config)
+            dsd_ds.close()
+            file_list = ImageDataGenerator()
+            i = 0
+            img = file_list.flow_from_directory('/app/imgs', target_size=(96, 128))
+            out_predict = model.predict(img)
             for i in range(len(out_predict)):
-                tstamp = int(scp['time_bins'][i].timestamp() * 1e9)
-                print(str(
-                    scp['time_bins'][i]) + ':' + class_names[int(out_predict[i])])
+                tstamp = int(time_list[i] * 1e9)
+                logging.debug(str(
+                       scp['time_bins'][i]) + ':' + class_names[int(out_predict[i])])
                 plugin.publish("weather.classifier.class",
                             int(out_predict[i]),
                             timestamp=tstamp)
             if args.loop == False:
-                run = False     
-        dsd_ds.close() 
-         
- 
+                    run = False
+        dsd_ds.close()
+
+
 def main(args):
     if args.verbose:
         print('running in a verbose mode')
@@ -190,11 +235,11 @@ if __name__ == '__main__':
         action='store_true', help='Verbose')
     parser.add_argument(
         '--input', dest='input',
-        action='store', default='sgpdlacfC1.00',
+        action='store', default='sgpdlacfC1.a1',
         help='Path to input device or ARM datastream name')
     parser.add_argument(
         '--model', dest='model',
-        action='store', default='/app/modelsnrgt3.000000snrgt5.000000.json',
+        action='store', default='resnet50.hdf5',
         help='Path to model')
     parser.add_argument(
         '--interval', dest='interval',
@@ -205,13 +250,20 @@ if __name__ == '__main__':
     parser.add_argument('--no-loop', action='store_false')
     parser.set_defaults(loop=True)
     parser.add_argument(
-            '--config', dest='config', action='store', default='User5',
+            '--config', dest='config', action='store', default='dlacf',
             help='Set to User5 for PPI or Stare for VPTs')
     parser.add_argument('--date', dest='date', action='store',
                         default=None,
                         help='Date of record to pull in (YYYY-MM-DD)')
     parser.add_argument('--time', dest='time', action='store',
-                        default=None, help='Time of record to pull',
-                        type=ascii)
+                        default=None, help='Time of record to pull')
 
+    gpus = tf.config.experimental.list_physical_devices('GPU')
+    if gpus:
+        try:
+            tf.config.experimental.set_virtual_device_configuration(
+                    gpus[0], [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=1024)])
+        except RuntimeError as e:
+            print(e)
     main(parser.parse_args())
+                                            
